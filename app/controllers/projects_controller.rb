b7ea0d5709d9b03033695c9205965b079822d2fe
Spike Grobstein <spikegrobstein@mac.com>
2011-08-17 19:59:50 -0400

    beginnings of projects implementation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,53 @@
 class ProjectsController < ApplicationController
+  
+  def index
+    @projects = Project.all
+  end
+  
+  def show
+    redirect_to edit_project_path(params[:id])
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    
+    begin
+      @project.save!
+      flash[:notice] = "Created new project! w00t!"
+      redirect_to edit_project_path(@project)
+    rescue ActiveRecord::RecordInvalid
+      render :action => :new
+    end
+  end
+  
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    
+    begin
+      @project.update_attributes!(params[:project])
+      flash[:notice] = "Saved project!"
+      
+      redirect_to edit_project_path(@project)
+    rescue ActiveRecord::RecordInvalid
+      render :action => :edit
+    end
+  end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    
+    flash[:notice] = "Deleted project!"
+    
+    redirect_to projects_path
+  end
+  
 end
