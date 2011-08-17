973002dadd24f91275ac6e18c1bb1240c75a86b9
Spike Grobstein <spikegrobstein@mac.com>
2011-08-17 19:45:06 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SadistechAdmin::Application.config.secret_token = '1eb5ab6d8140457f5f003a9f22ffa69872fc26c5b0bcb9de35f036bb873df7d0d7d9c24c35129d52ec66bed82f75d7f5845d495fa6d3f17f7b76902b247ae315'
