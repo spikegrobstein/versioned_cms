57fba55d5befd1c61f92695995827474680069f5
Spike Grobstein <spikegrobstein@mac.com>
2011-08-17 19:51:11 -0400

    added Project model and projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SadistechAdmin::Application.routes.draw do
+  
+  resources :projects
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20110817234935_create_projects.rb
@@ -0,0 +1,17 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+
+      t.string :name
+      t.string :url
+      t.text :css
+      t.string :footer
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
