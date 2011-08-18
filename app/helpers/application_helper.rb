9f4f141ad63231b60c56ffe089e0ba852a6e8ae3
Spike Grobstein <spikegrobstein@mac.com>
2011-08-17 20:09:51 -0400

    implemented forms and shit for projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def form_text_field(form, field, title=nil)
+    render :partial => 'shared/form_text_field', :locals => { :form => form, :field => field, :title => title }
+  end
+  
+  def form_text_area(form, field, title=nil)
+    render :partial => 'shared/form_text_area', :locals => { :form => form, :field => field, :title => title }
+  end
+  
 end
