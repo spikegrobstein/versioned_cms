e212c687d96b4ac73e30f7004bc89c76c97877e7
Spike Grobstein <spikegrobstein@mac.com>
2011-08-19 18:15:23 -0500

    updated code to use new bootstrap css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,22 @@
 module ApplicationHelper
   
+  def extra_head( &block )
+    @extra_head = true
+    content_for :extra_head do
+      block.call
+    end
+  end
+  
   def markdown(text)  
     Redcarpet.new(text).to_html.html_safe  
   end
   
-  def form_text_field(form, field, title=nil)
-    render :partial => 'shared/form_text_field', :locals => { :form => form, :field => field, :title => title }
+  def form_text_field(form, field, options={})
+    render :partial => 'shared/form_text_field', :locals => { :form => form, :field => field, :options => options }
   end
   
-  def form_text_area(form, field, title=nil)
-    render :partial => 'shared/form_text_area', :locals => { :form => form, :field => field, :title => title }
+  def form_text_area(form, field, options={})
+    render :partial => 'shared/form_text_area', :locals => { :form => form, :field => field, :options => options }
   end
   
 end
