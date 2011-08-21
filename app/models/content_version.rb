a6747761defcbd099cd200316f2736908e356187
Spike Grobstein <spikegrobstein@mac.com>
2011-08-21 15:19:31 -0400

    added currently_published? method to content_version

--- app/models/content_version.rb
+++ app/models/content_version.rb
@@ -4,8 +4,13 @@ class ContentVersion < ActiveRecord::Base
   scope :active, where('not deleted')
   
   has_many :published_content_versions
+  has_many :publications, :through => :published_content_versions
   
   belongs_to :page
   
+  
+  def currently_published?
+    not publications.find_by_is_current(true).nil?
+  end
   
 end
