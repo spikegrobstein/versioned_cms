3a2581ec64faa116da107601f2159f5a723c7aa2
Spike Grobstein <spikegrobstein@mac.com>
2011-08-20 21:17:50 -0400

    if publishing root is not absolute, make it relative to Rails.root

--- config/initializers/publishing.rb
+++ config/initializers/publishing.rb
@@ -1,1 +1,6 @@
 PUBLISHING_CONFIG = YAML.load_file("#{Rails.root}/config/publishing.yml")[Rails.env]
+
+# if it's not an absolute path, make it relative to Rails.root
+unless PUBLISHING_CONFIG['location'].match /^\//
+  PUBLISHING_CONFIG['location'] = File.join(Rails.root, PUBLISHING_CONFIG['location'])
+end
