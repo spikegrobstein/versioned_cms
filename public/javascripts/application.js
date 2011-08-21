59bd7e4c7a749e94398da14985e66904d5484301
Spike Grobstein <spikegrobstein@mac.com>
2011-08-21 12:12:01 -0400

    working on getting the deleted items to hide/show at the press of a button.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+jQuery(function($) {
+  $('.show_all_btn').click(function() {
+    $(this).parent('table').toggleClass('show_all');
+    return false;
+  });
+});
