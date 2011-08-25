facce2de107d7640b9c6afd554961e3ccf3b10d1
Spike Grobstein <spikegrobstein@mac.com>
2011-08-25 09:38:30 -0400

    added a select to the nav_item form for selecting existing pages.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,12 @@
 module ProjectsHelper
+  
+  def project_pages(project)
+    pages = []
+    pages << [ 'Project Page', '::project_page::' ]
+    
+    project.pages.each { |p| pages << [ p.title, p.slug ] }
+    
+    pages
+  end
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,4 +7,18 @@ jQuery(function($) {
     $(this).parents('table').toggleClass('show_all');
     return false;
   });
+  
+  
+  var original_nav_item_url = $('#nav_item_url').val();
+  $('#nav_item_url_select_').change(function() {
+    var new_val = '';
+    if ($(this).val() == '') {
+      new_val = original_nav_item_url
+    } else {
+      new_val = $(this).val();
+    }
+    
+    $('#nav_item_url').val(new_val);
+    
+  });
 });
